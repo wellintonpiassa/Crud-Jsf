62179576dd629d8dde8b0e1145a9ae0df4436057
Wellinton Piassa <51731873+wellintonpiassa@users.noreply.github.com>
2021-10-24 20:47:05 -0300

    Update ConnectionFactory.java

--- src/model/ConnectionFactory.java
+++ src/model/ConnectionFactory.java
@@ -10,12 +10,7 @@ public class ConnectionFactory {
 		try {
 			
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			 return DriverManager.getConnection(
-	                    "jdbc:mysql://35.223.159.189:3306/"
-	                    + "produtos?useTimezone=true&serverTimezone=America/"
-	                    + "Sao_Paulo" 
-	                    + "&allowPublicKeyRetrieval=true&useSSL=false",
-	                    "aluno", "aluno2021");
+			 return DriverManager.getConnection(/*Put connection with DB*/);
 			
 		} catch (Exception e) {
 			throw new RuntimeException(e);
